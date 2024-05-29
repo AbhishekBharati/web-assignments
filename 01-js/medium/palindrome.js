443aa687859fc7ebcb1b08b20db8b6223e1177ec
AbhishekBharati <210305105048@paruluniversity.ac.in>
2024-05-30 00:55:23 +0530

    Wrote the logic of Palindrome

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -4,6 +4,15 @@
 */
 
 function isPalindrome(str) {
+  str = str.toLowerCase().replace(/[\s\W_]+/g, '');
+  let n = Math.floor(str.length / 2);
+  let n1 = str.length;
+  console.log(str);
+  for (let i = 0; i < n; i++) {
+    if (str[i] != str[n1 - i - 1]) {
+      return false;
+    }
+  }
   return true;
 }
 
